c966ee5126f09d35a942f4ef60b3401615972932
marloncz <m.c.schumacher@live.de>
2023-02-25 12:23:33 +0100

    feat: added argument for performing parallel

--- R/get_predictive_power_scores.R
+++ R/get_predictive_power_scores.R
@@ -5,17 +5,23 @@
 #' @param top_n number of features as `int` that should be returned based on
 #' predictive power score. If `NULL` all features will be returned with the
 #' corresponding predictive power score. Defualt is `NULL`.
-#'
+#' @param parallel `bool`, whether to perform score calls in parallel.
+#' 
 #' @return `data.frame` containing pps for all features.
 #' @export
-get_predictive_power_scores <- function(data, target, top_n = NULL) {
+get_predictive_power_scores <- function(
+    data, 
+    target, 
+    top_n = NULL, 
+    parallel = TRUE
+    ) {
   # selecting all numerical columns
   data <- data %>%
     dplyr::select_if(is.numeric) %>%
     ggplot2::remove_missing(na.rm = TRUE)
   
   # get scores
-  scores <- ppsr::score_predictors(df = data, y = target, do_parallel = FALSE)
+  scores <- ppsr::score_predictors(df = data, y = target, do_parallel = parallel)
   
   # shaping results
   out <- scores %>%
